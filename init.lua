bceb4b9568487f4c68e5fbb85a8194c165aa9a47
Chris Cullinane <chriscullinane@protonmail.com>
2024-01-22 22:50:51 -0900

    adds copilot

--- init.lua
+++ init.lua
@@ -248,6 +248,8 @@ require('lazy').setup({
     build = ':TSUpdate',
   },
 
+  'github/copilot.vim',
+
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
